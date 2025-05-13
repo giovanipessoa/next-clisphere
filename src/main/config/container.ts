49a090b2cb6d3a2732cff0a28723c77ccd3f3b5e
DEXS <contato.dexs@gmail.com>
2025-05-13 17:36:53 -0300

    fix: fix import

--- src/main/config/container.ts
+++ src/main/config/container.ts
@@ -1,13 +1,15 @@
 import { PrismaClientRepository } from "../../infra/repositories/prisma-client-repository";
-import { CreateClientUseCase } from "../../domain/usecases/create-client";
+import { CreateClientUseCase } from "../../domain/usecases/client/create-client";
 import { PrismaEventRepository } from "../../infra/repositories/prisma-event-repository";
-import { CreateEventUseCase } from "../../domain/usecases/create-event";
+import { CreateEventUseCase } from "../../domain/usecases/event/create-event";
 
+// Client
 export const makeCreateClientUseCase = () => {
     const repository = new PrismaClientRepository();
     return new CreateClientUseCase(repository);
 };
 
+// Event
 export const makeCreateEventUseCase = () => {
     const repository = new PrismaEventRepository();
     return new CreateEventUseCase(repository);
